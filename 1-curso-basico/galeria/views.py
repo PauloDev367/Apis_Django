befc36061e2571d9a49663a2976ebeebc8e5c45a
Paulo Silva <pauloecomercial367@gmail.com>
2024-04-08 20:39:36 -0300

    :sparkles: Nomes dinamicos

--- 1-curso-basico/galeria/views.py
+++ 1-curso-basico/galeria/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
+dados = {
+    1: {
+        "nome": "Nebulosa de Carina",
+        "legenda": "webbtelecope.org/NASA/James Webb"    
+    }, 
+    2: {
+        "nome": "Galáxia NGC 1079",
+        "legenda": "Qualquer coisa"
+    },
+
+}
 
 def index(request):
-    return render(request, "galeria/index.html")
+    return render(request, "galeria/index.html", {"cards": dados})
 
 def imagem(request):
     return render(request, "galeria/imagem.html")
