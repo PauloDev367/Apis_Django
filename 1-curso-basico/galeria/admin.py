7e8e89cee860994bb210235311973eed6a9d3118
Paulo Silva <pauloecomercial367@gmail.com>
2024-04-11 08:05:11 -0300

    :sparkles: NO campo e editando o campo na listagem de dados

--- 1-curso-basico/galeria/admin.py
+++ 1-curso-basico/galeria/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda", "categoria")
+    list_display = ("id", "nome", "legenda", "categoria", "publicada")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
     list_filter = ("categoria",)
-    list_per_page=1
+    list_per_page= 10
+    list_editable=("publicada",)
 
 admin.site.register(Fotografia, ListandoFotografias)
