695521d3030e7b42ec5995423eeac1ff99ee1f4c
Paulo Silva <pauloecomercial367@gmail.com>
2024-04-11 07:56:03 -0300

    :sparkles: Nova coluna de fotografias

--- 1-curso-basico/galeria/admin.py
+++ 1-curso-basico/galeria/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "categoria")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
 
--- 1-curso-basico/galeria/models.py
+++ 1-curso-basico/galeria/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 # Django ORM
 class Fotografia(models.Model):
+    # precisa ser uma tupla para o Charfield entender
+    OPCOES_CATEGORIA = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALÁXIA", "Galáxia"),
+        ("PLANETA", "Planeta"),
+    ]
+    
     nome = models.CharField(max_length=100, null=False, blank=False) # coluna do tipo string
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
     
