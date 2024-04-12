f7edf4ea1fbe9bafd920f787db8f70dc9581b9f7
Paulo Silva <pauloecomercial367@gmail.com>
2024-04-12 08:41:26 -0300

    :sparkles: Base sobre templates

--- 3-udemy/recipes/views.py
+++ 3-udemy/recipes/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("HOME")
+    return render(request, "home.html", context={"name": "Paulo"})
 
 def contato(request):
     return HttpResponse("CONTATO")
