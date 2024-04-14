fc3d53443f0806ea5d785b6d6136133506e082b8
Paulo Silva <pauloecomercial367@gmail.com>
2024-04-14 11:24:05 -0300

    :sparkles: Registrando category e recipe no django admin

--- 3-udemy/recipes/admin.py
+++ 3-udemy/recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from recipes.models import Recipe
+from recipes.models import Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id","name")
+    
+class RecipesAdmin(admin.ModelAdmin):
+    list_display = ("title","description","slug","preparation_time","preparation_time_unit","servings","servings_unit","preparation_step","preparation_step_is_html","created_at","updated_at","is_published","cover","category","author",)
+
+admin.site.register(Recipe, RecipesAdmin)
+admin.site.register(Category, CategoryAdmin)
