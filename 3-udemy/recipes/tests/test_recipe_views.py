73c52e434e9433dece99a5a94815202e3b0771de
Paulo <pauloecomercial367@gmail.com>
2024-04-22 20:20:31 -0300

    :sparkles: Seprando os testes em um package chamado tests com __init__.py

--- 3-udemy/recipes/tests/test_recipe_views.py
+++ 3-udemy/recipes/tests/test_recipe_views.py
@@ -2,23 +2,6 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
 
-class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert 1 == 1, '1 é igual a 1'
-    
-    def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
-    
-    def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(url, '/recipes/category/1/')
-        
-    def test_recipe_detail_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, '/recipes/1/')
-        
-    
 class RecipeViewsTest(TestCase):
     def test_recipe_home_views_functions_is_correct(self):
         # qual função está sendo usada por uma url
