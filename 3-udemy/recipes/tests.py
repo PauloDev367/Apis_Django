02f2d602b7de24aef8c2d08a2bb3bbfe6fd0aec5
Paulo <pauloecomercial367@gmail.com>
2024-04-22 19:28:35 -0300

    :sparkles: Testando qual função está sendo usada pela url

--- 3-udemy/recipes/tests.py
+++ 3-udemy/recipes/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from recipes import views
 
 class RecipeURLsTest(TestCase):
     def test_the_pytest_is_ok(self):
@@ -16,3 +17,18 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
+        
+    
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_views_functions_is_correct(self):
+        # qual função está sendo usada por uma url
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+    
+    def test_recipe_category_views_functions_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+        
+    def test_recipe_detail_views_functions_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)
