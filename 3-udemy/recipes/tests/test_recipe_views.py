38c82260f5252b26bb4eb93c55619b101e62e6ee
Paulo <pauloecomercial367@gmail.com>
2024-04-23 08:50:59 -0300

    :sparkles: Testando context da response no template

--- 3-udemy/recipes/tests/test_recipe_views.py
+++ 3-udemy/recipes/tests/test_recipe_views.py
@@ -1,20 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 class RecipeViewsTest(TestCase):
     def test_recipe_home_views_functions_is_correct(self):
         # qual função está sendo usada por uma url
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
-    
-    def test_recipe_category_views_functions_is_correct(self):
-        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
-        self.assertIs(view.func, views.category)
-        
-    def test_recipe_detail_views_functions_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
-        self.assertIs(view.func, views.recipe)
     
     def test_recipe_home_view_returns_status_code_200_ok(self):
         response = self.client.get(reverse('recipes:home'))
@@ -30,3 +23,48 @@ class RecipeViewsTest(TestCase):
             'No recipes found',
             response.content.decode('utf-8')
         )
+    
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category = category,
+            author = author,
+            title = 'Recipe Title',
+            description = 'Recipe Description',
+            slug = 'recipe-slug',
+            preparation_time = 10,
+            preparation_time_unit = 'minutes',
+            servings = 5,
+            servings_unit = 'porções',
+            preparation_step = 'Recipe preparation steps',
+            preparation_step_is_html = True,
+            is_published = True,
+        )
+        
+        response = self.client.get(reverse('recipes:home'))
+        response_recipes = response.context['recipes']
+        self.assertEqual(response_recipes.first().title, 'Recipe Title')
+        
+        
+    def test_recipe_category_views_functions_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1000}))
+        self.assertIs(view.func, views.category)
+    
+    def test_recipe_category_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1000}))
+        self.assertEqual(response.status_code, 404)    
+    
+    def test_recipe_detail_views_functions_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1000}))
+        self.assertEqual(response.status_code, 404)    
