ac359895ed8e79eb8be6e8563dd391b618b070c4
Paulo <pauloecomercial367@gmail.com>
2024-04-26 08:15:05 -0300

    :sparkles: Generic views

--- 4-api-youtube/cursos/views.py
+++ 4-api-youtube/cursos/views.py
@@ -1,29 +1,22 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializers, AvaliacaoSerializer
 
-class CursoAPIView(APIView):
-    def get(self, request):
-        cursos = Curso.objects.all()
-        serializer = CursoSerializers(cursos, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = CursoSerializers(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-class AvaliacaoAPIView(APIView):
-    def get(self, request):
-        avaliacoes = Avaliacao.objects.all()
-        serializer = AvaliacaoSerializer(avaliacoes, many=True)
-        return Response(serializer.data)
 
-    def post(self, request):
-        serializer = AvaliacaoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+# para metodos get e post
+class CursosAPIView(generics.ListCreateAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializers
+    
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializers
+
+
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
