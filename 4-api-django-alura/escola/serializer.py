41c836a864cf014cfc3b0cbe0b9a20145af1cf51
Paulo <pauloecomercial367@gmail.com>
2024-04-27 17:30:09 -0300

    :sparkles: Configurando serializers

---
+++ 4-api-django-alura/escola/serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from escola.models import Aluno, Curso
+
+
+# Segue o mesmo principio dos ViewModel do C#, servem apenas para fazer a ponte do json
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = '__all__'
