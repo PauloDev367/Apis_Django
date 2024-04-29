2bb59ab0be3f2945bd6f483be8ac8fcc21276bd1
Paulo <pauloecomercial367@gmail.com>
2024-04-29 18:48:55 -0300

    :sparkles: Boas praticas de validacao

--- 4-api-youtube/cursos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CursosAPIView,CursoAPIView, AvaliacoesAPIView, AvaliacaoAPIView
-
-urlpatterns = [
-    path("cursos/", CursosAPIView.as_view(), name='cursos'),
-    path("cursos/<int:pk>", CursoAPIView.as_view(), name='cursos'),
-    path("avaliacoes/", AvaliacoesAPIView.as_view(), name='avaliacoes'),
-    path("avaliacoes/<int:pk>", AvaliacaoAPIView.as_view(), name='avaliacoes'),
-]
