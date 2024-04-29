4cb33ae65e57322933860452484af128e50c76e8
Paulo <pauloecomercial367@gmail.com>
2024-04-29 18:54:35 -0300

    :sparkles: Validando com regex

--- 5-api-django-alura-validacoes-buscas-filtros-deploy/clientes/serializers.py
+++ 5-api-django-alura-validacoes-buscas-filtros-deploy/clientes/serializers.py
@@ -10,11 +10,11 @@ class ClienteSerializer(serializers.ModelSerializer):
         if not cpf_valido(data['cpf']):
             raise serializers.ValidationError({'cpf': "O CPF deve ter 11 dígitos"})
         if not nome_valido(data['nome']):
-            raise serializers.ValidationError("Não inclua números nesse campo")
+            raise serializers.ValidationError({"nome":"Não inclua números nesse campo"})
         if not rg_valido(data['rg']):
-            raise serializers.ValidationError("O RG deve ter 9 dígitos")
+            raise serializers.ValidationError({"rg":"O RG deve ter 9 dígitos"})
         if not celular_valido(data['celular']):
-            raise serializers.ValidationError("O celular deve ter 11 dígitos")
+            raise serializers.ValidationError({"celular":"O celular deve seguir o padrão 11 91234-1234"})
 
         return data
     
--- 5-api-django-alura-validacoes-buscas-filtros-deploy/clientes/validators.py
+++ 5-api-django-alura-validacoes-buscas-filtros-deploy/clientes/validators.py
@@ -1,3 +1,6 @@
+import re
+
+
 def cpf_valido(numero_cpf):
     return len(numero_cpf) != 11
 
@@ -8,4 +11,7 @@ def rg_valido(rg):
     return len(rg) != 9
 
 def celular_valido( celular):
-    return len(celular) < 11
+    """Verifica se o celular é válido (11 91234-1234)"""
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta = re.findall(modelo, celular)
+    return resposta   
