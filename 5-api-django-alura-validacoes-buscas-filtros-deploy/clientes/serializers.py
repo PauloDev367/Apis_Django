13388a076bbaa48550dad76df3fdd835115ef544
Paulo <pauloecomercial367@gmail.com>
2024-04-29 18:46:19 -0300

    :sparkles: Boas praticas de validacao

--- 5-api-django-alura-validacoes-buscas-filtros-deploy/clientes/serializers.py
+++ 5-api-django-alura-validacoes-buscas-filtros-deploy/clientes/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf': "O CPF deve ter 11 dígitos"})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError("Não inclua números nesse campo")
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError("O RG deve ter 9 dígitos")
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError("O celular deve ter 11 dígitos")
+
+        return data
     
-    def validate_cpf(self,cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError("O CPF deve ter 11 dígitos")
+    
